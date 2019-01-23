c516c7d52ff8460b51b2883e6fc7fad8ece02675
Mashukou_M <1nikmsh1@gmail.com>
2019-01-23 14:15:08 +0300

    edited working with localStorage

--- index.js
+++ index.js
@@ -1,75 +1,86 @@
+const keys = ["todayList", "tomorrowList", "nextTimeList"];
 
 window.onload = function() {
-    let taskId = 0;
 
-    //getDataFromLocalStorage();
+    getDataFromLocalStorage();
 
-    document.getElementById('taskInput').addEventListener("keyup", function (event) {
-            event.preventDefault();
-            if (event.keyCode === 13) {
-                document.getElementById("addTask").click();
-            }
-            if (event.keyCode === 40) {
-                let select = document.getElementById("selectDay");
-                if (select.options.selectedIndex < select.options.length-1)  {
-                    select.options.selectedIndex++;
-                } else {
-                    select.options.selectedIndex = 0;
-                }
-
-            }
-            if (event.keyCode === 38) {
-                let select = document.getElementById("selectDay");
-                if (select.options.selectedIndex>0) {
-                    select.options.selectedIndex--;
-                } else {
-                    select.options.selectedIndex = select.options.length-1;
-                }
-
-            }
-
-        }
-    );
-
+    document.getElementById('taskInput').addEventListener("keyup", listenInputKeys);
     document.getElementById('addTask').onclick = addItemToList;
     document.getElementById('remTasks').onclick = removeTasks;
     document.getElementById('clearTasks').onclick = clearTasks;
 
-    function addItemToList() {
-        let taskName = document.getElementById("taskInput").value;
-        if (taskName === "") return;
-
-        let selectedDay = document.getElementById('selectDay').value;
-        let formGroup = document.getElementById(selectedDay + "List");
-
-        let div = document.createElement("div");
-        div.id = "div" + taskId;
-        div.className = "taskDiv";
-
-        let taskCheckBox = document.createElement("input");
-        taskCheckBox.type = "checkbox";
-        taskCheckBox.id = taskId++;
-
-        let taskLabel = document.createElement("label");
-        taskLabel.htmlFor = taskCheckBox.id;
-        taskLabel.innerText = taskName;
-
-        div.appendChild(taskCheckBox);
-        div.appendChild(taskLabel);
-        formGroup.appendChild(div);
-
-        console.log(div);
-        //addDataToLocalStorage(selectedDay + "List", div.value.toString());
-        //document.getElementById("taskInput").value = "";
-    }
 };
+
+function addItemToList() {
+    const taskName = document.getElementById("taskInput").value;
+    if (taskName === "") return;
+
+    const taskId = generateId();
+
+    let selectedDay = document.getElementById('selectDay').value;
+    const listId = selectedDay + "List";
+
+
+    document.getElementById(listId).appendChild(createItem(taskId, taskName));
+    addDataToLocalStorage(listId, {id: taskId, text: taskName});
+}
+
+function listenInputKeys(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("addTask").click();
+    } else if (event.keyCode === 40) {
+        let select = document.getElementById("selectDay");
+        if (select.options.selectedIndex < select.options.length-1)  {
+            select.options.selectedIndex++;
+        } else {
+            select.options.selectedIndex = 0;
+        }
+
+    } else if (event.keyCode === 38) {
+        let select = document.getElementById("selectDay");
+        if (select.options.selectedIndex>0) {
+            select.options.selectedIndex--;
+        } else {
+            select.options.selectedIndex = select.options.length-1;
+        }
+
+    }
+
+}
 
 function removeTasks() {
     let checkboxes = document.querySelectorAll('#toDoListTable input[type=checkbox]:checked');
 
     for (let i = 0; i < checkboxes.length; i++) {
-        let div = document.getElementById("div"+checkboxes[i].id);
+        const objId = checkboxes[i].id;
+        console.log(objId);
+        const div = document.getElementById("div"+ objId);
         div.parentNode.removeChild(div);
+        remFromLocalStorage(objId);
+    }
+}
+
+function remFromLocalStorage(id) {
+    console.log('working ' + id);
+    const size = keys.length;
+    for (let i=0; i<size; i++) {
+        let array = JSON.parse(localStorage.getItem(keys[i]));
+        if (array===null || array.length === 0) continue ;
+
+        console.log(array);
+        for (let j=0; j<array.length; j++) {
+            console.log(array[j].id + " === " + id);
+            if (array[j].id === id) {
+                console.log("FOUND");
+                array.splice(array[j], 1);
+                console.log("Works! " + array);
+                console.log(JSON.stringify(array));
+                localStorage.setItem(keys[i], JSON.stringify(array));
+                return ;
+            }
+        }
+        //document.getElementById(key).appendChild(fragment);
     }
 }
 
@@ -80,40 +91,53 @@ function clearTasks() {
         let div = document.getElementById("div"+checkboxes[i].id);
         div.parentNode.removeChild(div);
     }
-    localStorage.clear()
+    localStorage.clear();
 }
-
-function saveToLocalStorage(taskSection, tasksArray) {
-    if (tasksArray.length === 0) {
-        return ;
-    }
-    localStorage.setItem(taskSection, JSON.stringify(tasksArray));
-}
-
 
 function getDataFromLocalStorage() {
-    let keys = ["todayList", "tomorrowList", "nextTimeList"];
     keys.forEach(function (key) {
             let array = JSON.parse(localStorage.getItem(key));
             if (array===null || array.length === 0) return ;
 
-            let arrPlace = document.getElementById(key);
-            console.log(array);
+            let fragment = document.createDocumentFragment();
             for (let i=0; i<array.length; i++) {
-                console.log(array[i]);
-                arrPlace.appendChild(array[i]);
+                fragment.appendChild(createItem(array[i].id, array[i].text));
             }
+            document.getElementById(key).appendChild(fragment);
         }
     );
 }
 
 function addDataToLocalStorage(key, item) {
     let tasks = JSON.parse(localStorage.getItem(key));
-    console.log(tasks);
     if (tasks === null) {
         tasks = [];
     }
-    console.log(item);
     tasks.push(item);
     localStorage.setItem(key, JSON.stringify(tasks));
+}
+
+function createItem(id, text) {
+    if (text === "" || id==="") return;
+
+    let div = document.createElement("div");
+    div.id = "div" + id;
+    div.className = "taskDiv";
+
+    let taskCheckBox = document.createElement("input");
+    taskCheckBox.type = "checkbox";
+    taskCheckBox.id = id;
+
+    let taskLabel = document.createElement("label");
+    taskLabel.htmlFor = id;
+    taskLabel.innerText = text;
+
+    div.appendChild(taskCheckBox);
+    div.appendChild(taskLabel);
+
+    return div;
+}
+
+function generateId () {
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
